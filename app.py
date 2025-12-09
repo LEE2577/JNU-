cf75e250c388d327530646746a9b1e845cebcbd2
LEE2577 <cy.lee2577@gmail.com>
2025-12-09 18:22:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'  # 请更改为安全的密钥
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/agewell'
+app.config['MONGO_URI'] = 'mongodb+srv://cylee2577_db_user:Chengyu2577@agewell.tuqhti5.mongodb.net/agewell_db?retryWrites=true&w=majority&appName=agewell'
 
 mongo = PyMongo(app)
 
